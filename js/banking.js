bedc6270ee92a5a0e414c749e29b735c3ba14280
Faysal <74490913+faysalchowdhury99@users.noreply.github.com>
2021-08-16 15:31:29 +0600

    added functionality

--- js/banking.js
+++ js/banking.js
@@ -1,86 +1,70 @@
 // selectors
 const depositForm = document.getElementById('deposit-form');
 const withdrawForm = document.getElementById('withdraw-form');
+const depositField = document.getElementById('deposit-field');
+const withdrawField = document.getElementById('withdraw-field');
+const totalDepositAmountBox = document.getElementById('total-deposit-amount');
+const totalWithdrawAmountBox = document.getElementById('total-withdraw-amount');
+const totalBalanceBox = document.getElementById('total-balance');
 
 // functions
+function getInputValue(inputField) {
+  const inputFieldText = inputField.value;
+  const inputValue = parseFloat(inputFieldText);
+  inputField.value = '';
+  return inputValue;
+}
+
+function updateBalance(amountBox, newAmount) {
+  const currentBalanceText = amountBox.innerText;
+  const currentBalance = parseFloat(currentBalanceText);
+  const newBalance = currentBalance + newAmount;
+  amountBox.innerText = newBalance;
+}
+
+function getCurrentTotalBalance() {
+  const totalBalanceBoxText = totalBalanceBox.innerText;
+  const totalBalance = parseFloat(totalBalanceBoxText);
+  return totalBalance;
+}
+
+function updateTotalBalance(newAmount, isAdd) {
+  const currentTotalBalance = getCurrentTotalBalance();
+  let newTotalBalance;
+  if (isAdd == true) {
+    newTotalBalance = currentTotalBalance + newAmount;
+  } else {
+    newTotalBalance = currentTotalBalance - newAmount;
+  }
+  totalBalanceBox.innerText = newTotalBalance;
+}
+
 function handleDeposit(event) {
   event.preventDefault();
-  // get deposit value
-  const depositField = document.getElementById('deposit-field');
-  const newDepositAmount = parseFloat(depositField.value);
-
+  const newDepositAmount = getInputValue(depositField);
   if (newDepositAmount <= 0 || isNaN(newDepositAmount)) {
-    alert('Deposit amount must be grater than 0');
+    alert('Deposit ammount must be greater than 0');
     return;
   }
-
-  // update deposit balance
-  const totalDepositAmountBox = document.getElementById('total-deposit-amount');
-  const totalDepositAmountText = totalDepositAmountBox.innerHTML;
-  const totalDepositAmount = parseFloat(totalDepositAmountText);
-
-  totalDepositAmountBox.innerText = (
-    totalDepositAmount + newDepositAmount
-  ).toFixed(2);
-
-  // update total balance
-  const totalBalanceBox = document.getElementById('total-balance');
-  const totalBalanceText = totalBalanceBox.innerHTML;
-  const totalBalance = parseFloat(totalBalanceText);
-
-  totalBalanceBox.innerText = (totalBalance + newDepositAmount).toFixed(2);
-
-  // clear
-  depositField.value = '';
+  updateBalance(totalDepositAmountBox, newDepositAmount);
+  updateTotalBalance(newDepositAmount, true);
 }
 
 function handleWithdraw(event) {
   event.preventDefault();
-  // get withdraw value
-  const withdrawField = document.getElementById('withdraw-field');
-  const newWithdrawAmount = parseFloat(withdrawField.value);
-
-  console.log(newWithdrawAmount);
-
+  const currentTotalBalance = getCurrentTotalBalance();
+  const newWithdrawAmount = getInputValue(withdrawField);
   if (newWithdrawAmount <= 0 || isNaN(newWithdrawAmount)) {
-    alert('Withdraw amount must be greater than 0');
+    alert('Withdraw ammount must be greater than 0');
+    return;
+  } else if (newWithdrawAmount > currentTotalBalance) {
+    alert("You don't have enought money to withdraw");
     return;
   }
-
-  // update withdraw balance
-  const totalWithdrawAmountBox = document.getElementById(
-    'total-withdraw-amount'
-  );
-  const totalWithdrawAmountText = totalWithdrawAmountBox.innerHTML;
-  const totalWithdrawAmount = parseFloat(totalWithdrawAmountText);
-
-  totalWithdrawAmountBox.innerText = (
-    totalWithdrawAmount + newWithdrawAmount
-  ).toFixed(2);
-
-  // update total balance
-  const totalBalanceBox = document.getElementById('total-balance');
-  const totalBalanceText = totalBalanceBox.innerHTML;
-  const totalBalance = parseFloat(totalBalanceText);
-
-  totalBalanceBox.innerText = (totalBalance - newWithdrawAmount).toFixed(2);
-
-  // clear
-  withdrawField.value = '';
+  updateBalance(totalWithdrawAmountBox, newWithdrawAmount);
+  updateTotalBalance(newWithdrawAmount, false);
 }
 
 // event listeners
 depositForm.addEventListener('submit', handleDeposit);
 withdrawForm.addEventListener('submit', handleWithdraw);
-
-// Greet
-const hrs = new Date().getHours();
-const greetMessage = document.getElementById('greet');
-let greet = 'Good Evening';
-
-if (hrs < 12) greet = 'Good Morning';
-else if (hrs >= 12 && hrs <= 17) greet = 'Good Afternoon';
-else if (hrs >= 17 && hrs <= 20) greet = 'Good Evening';
-else if (hrs >= 20 && hrs <= 24) greet = 'Good Night';
-
-greetMessage.innerText = greet;
